90b4a071f1c243e004d2a0494108cd58aac3c068
Alisha Jesani <alishajesani@Mac.lan>
2025-03-13 00:24:28 -0700

    feat: added the container and and score and timer for resultpage issue #5

--- src/pages/Resultpage.js
+++ src/pages/Resultpage.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Resultpage.css'; // Ensure to create and style this file accordingly
 
 const ResultPage = () => {
+
   return (
     <div className="result-container">
-      <header>Quiz Results</header>
-      <p>Your quiz results will appear here...</p>
+      <div className="result-header">
+        <div className="timer-box">Timer:</div>
+        <div className="score-box">Score:</div>
+      </div>
+      
+      <div className="result-content">
+        <div className="score-message">Score:</div>
+        <div className="result-info">
+          <p>Congratulations! You are officially BrainGoated!</p>
+        </div>
+        
+        
+      </div>
+      
+    
     </div>
   );
 };
