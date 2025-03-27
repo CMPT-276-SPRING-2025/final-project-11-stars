5795b2841aef38e96099683b539fafbca3baa2e7
Beyzanur Kuyuk <bka81@sfu.ca>
2025-03-27 13:23:07 -0700

    style: create and add animated logo of Bud-E - issue #13

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,7 +6,14 @@ const HomePage = () => {
   return (
     <div className="landing-page">
       <h1 className="main-heading">BrainGoated</h1>
-      <img src="/bud-e.png" alt="BrainGoated Logo" className="logo" />
+      {/* <img src="/bud-e.png" alt="BrainGoated Logo" className="logo" /> */}
+      <div className="iframe-wrapper">
+        <iframe
+          src="/BudE_animation.html"
+          title="Bud-E Animation"
+          className="bud-e-frame"
+        />
+      </div>
       <p className="tagline">Water your curiosity, Watch it grow!</p>
       <div className="quiz-category-space"></div>
 
