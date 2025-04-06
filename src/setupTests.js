a749dc8f3cc46820828d310fc22646d7cf6fc403
gurpreetkkalra <gurpreet.kaur.kalra18@gmail.com>
2025-04-06 00:17:17 -0700

    chore: suppress React Router warnings in console

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (
+    args[0] &&
+    (args[0].includes("React Router Future Flag Warning") ||
+     args[0].includes("Relative route resolution within Splat routes"))
+  ) {
+    return;
+  }
+  originalWarn(...args);
+};
